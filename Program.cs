9c680c6a065585bedd187e269e905359a9e7a644
riham <riham91.alsiyabi@gmail.com>
2025-04-08 13:53:01 +0400

    Exit

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ namespace SimpleHotelRoomManagementProject_2
                     Console.WriteLine("5. Search reservation by guest name..");
                     Console.WriteLine("6. Find the highest-paying guest..");
                     Console.WriteLine("7. Cancel a reservation by room number.. ");
-                    Console.WriteLine("0. Exit ");
+                    Console.WriteLine("0. Exit.. ");
                     Console.Write("Enter The Number Of Feature: ");
 
                     choiceNum = int.Parse(Console.ReadLine());
