a89f497a924d42ff976724d062d0049cd3cea7f5
riham <riham91.alsiyabi@gmail.com>
2025-04-08 13:52:39 +0400

    Cancel a reservation by room number

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ namespace SimpleHotelRoomManagementProject_2
                     Console.WriteLine("4. View all reservations with total cost..");
                     Console.WriteLine("5. Search reservation by guest name..");
                     Console.WriteLine("6. Find the highest-paying guest..");
-                    Console.WriteLine("7. Cancel a reservation by room number ");
+                    Console.WriteLine("7. Cancel a reservation by room number.. ");
                     Console.WriteLine("0. Exit ");
                     Console.Write("Enter The Number Of Feature: ");
 
